a9233f72221da7360029c1c2c924abf041823bbc
Hermia Tian <hermia1108@gmail.com>
2021-06-24 11:05:27 -0700

    q4 finished

--- q4.c
+++ q4.c
@@ -4,5 +4,12 @@ void matrix_vector_multiplication(
     int32_t num_colums,
     const int16_t*input,
     int16_t*output,
-
+    
+    int i, j;
+    for(i = 0;i< num_rows ;i++){
+        output[i]=0;
+        for(j = 0; j< num_colums;j++){
+            output[i] += matrix[i][j]*input[j];
+        }
+    }
 );
